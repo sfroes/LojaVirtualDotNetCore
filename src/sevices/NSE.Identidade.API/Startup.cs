22576340595fc57b29baf926726317f468655ee3
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-09-21 19:03:33 -0300

    Implementação da documentação swagger

--- src/sevices/NSE.Identidade.API/Startup.cs
+++ src/sevices/NSE.Identidade.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
 using System;
 using System.Collections.Generic;
@@ -37,11 +38,27 @@ namespace NSE.Identidade.API
                 .AddDefaultTokenProviders();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "NerdStore Enterpise Identity API",
+                    Description = "Esta API faz parte do curso de ASP.NET Core Enterpise Applications.",
+                    Contact = new OpenApiContact() { Name = "Froes Filho", Email = "sfroes.filho@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
