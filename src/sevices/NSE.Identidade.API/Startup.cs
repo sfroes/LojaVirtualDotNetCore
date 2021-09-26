e09e1101f2eaf4e944bfdc317d8b95f84dc07fac
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-09-26 00:37:27 -0300

    Configuração do launch e do appsettings

--- src/sevices/NSE.Identidade.API/Startup.cs
+++ src/sevices/NSE.Identidade.API/Startup.cs
@@ -23,14 +23,25 @@ namespace NSE.Identidade.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                                         .SetBasePath(hostEnvironment.ContentRootPath)
+                                         .AddJsonFile("appsettings.json", true, true)
+                                         .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                                         .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+
+        }        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
