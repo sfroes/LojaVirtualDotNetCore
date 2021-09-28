adfc7981f081e67300063d1b9bb59071a12f4b15
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-09-27 22:38:19 -0300

    Configuração do Identity para uso de autenticação

---
+++ src/web/NSE.WebApp.MVC/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(option => {
+                        option.LoginPath = "/login";
+                        option.AccessDeniedPath = "/acesso-negado";
+                    });
+            
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
