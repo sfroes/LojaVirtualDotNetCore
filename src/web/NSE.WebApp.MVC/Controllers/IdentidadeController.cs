f9ef0eb51553de103ba5691d4d0e0e2d79ed4833
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-09-27 23:23:39 -0300

    Configuração da controller basica e criação das paginas de login e registro

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,13 +1,69 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using NSE.WebApp.MVC.Models.Usuario;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(usuarioRegistro);
+            }
+
+            // Api - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar Login na app
+
+            return RedirectToAction("Index", "Home");
+            
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(usuarioLogin);
+            }
+
+            // Api - Login
+
+            if (false) return View(usuarioLogin);
+
+            //Realizar Login na app
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+
+        }
 
     }
 }
