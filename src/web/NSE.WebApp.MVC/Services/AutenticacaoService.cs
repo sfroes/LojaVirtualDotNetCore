38f87bffa889a973298037b5d15656f2b6c04689
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-10-05 21:51:31 -0300

    Criação das classe e a injeção de depencia

---
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,35 @@
+﻿using NSE.WebApp.MVC.Models.Usuario;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContente = new StringContent(JsonSerializer.Serialize(usuarioLogin),Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:44396/api/identidade/auttenticar", loginContente);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<string> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            var registroContente = new StringContent(JsonSerializer.Serialize(usuarioRegistro), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:44396/api/identidade/nova-conta", registroContente);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
