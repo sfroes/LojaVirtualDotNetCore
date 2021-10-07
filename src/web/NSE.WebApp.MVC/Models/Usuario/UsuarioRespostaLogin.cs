60837b5668eee1184be09613d3c738a37d32134d
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-10-06 21:18:39 -0300

    Criação dos modelos de resposta

---
+++ src/web/NSE.WebApp.MVC/Models/Usuario/UsuarioRespostaLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Models.Usuario
+{
+    public class UsuarioRespostaLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UsuarioToken UsuarioToken { get; set; }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Models/Usuario/UsuarioToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Models.Usuario
+{
+    public class UsuarioToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UsuarioClaim> Claims { get; set; }
+    }
+}
