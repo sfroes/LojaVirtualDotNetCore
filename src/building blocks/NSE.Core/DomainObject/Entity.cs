b74ce58a2342cfe46494163f3d63b38d4224098e
Serafim Francisco Froes Filho <34682247+sfroes@users.noreply.github.com>
2021-10-21 00:18:14 -0300

    Implementação da nossa classe Entity

---
+++ src/building blocks/NSE.Core/DomainObject/Entity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Core.DomainObject
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if(ReferenceEquals(this, compareTo))
+            {
+                return false;
+            }
+
+            if(ReferenceEquals(null, compareTo))
+            {
+                return false;
+            }
+
+            return Id.Equals(compareTo.Id);    
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if(ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+
+            if(ReferenceEquals( a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
